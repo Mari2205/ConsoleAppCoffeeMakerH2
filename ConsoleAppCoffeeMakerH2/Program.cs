d3feb7a02d3ee509639da0a33e8405dae93644de
Uncharted <uncha@LAPTOP-1PJR3MM8>
2020-11-11 21:18:51 +0100

    deleetd something

--- ConsoleAppCoffeeMakerH2/Program.cs
+++ ConsoleAppCoffeeMakerH2/Program.cs
@@ -11,70 +11,8 @@ namespace ConsoleAppCoffeeMakerH2
         static void Main(string[] args)
         {
             Gui gui = new Gui();
-
+          
             gui.ShowDrink();
-            //Console.WriteLine("How much is there of :");
-            //Console.Write("Water) ");
-            //int water = Convert.ToInt32(Console.ReadLine());
-            //Console.Write("Produkt) ");
-            //int produkt = Convert.ToInt32(Console.ReadLine());
-
-            //gui.GetTerminal();
-
-            //Console.WriteLine("Chose an number :");
-            //int userChooseDrink = int.Parse(Console.ReadLine());
-            //Console.Clear();
-
-            //switch (userChooseDrink)
-            //{
-            //    case 1:
-            //        HotDrink coffee = new CoffeeMachine(water, produkt, "Filter - coffee", "Coffee beanies", 96);
-            //        if (water <= 10 || produkt <= 7)
-            //        {
-            //            gui.Replenishment(coffee.CheckContents());
-            //        }
-            //        else
-            //        {
-            //            Console.WriteLine(coffee.MakeDrink());
-            //            Console.WriteLine(coffee.ServeDrink());
-            //        }
-            //        break;
-
-            //    case 2:
-            //        Console.Write("which type of Tea would you like : ");
-            //        string produktType = Console.ReadLine();
-            //        Console.Write("what temperature must the tea be : ");
-            //        int temp = Convert.ToInt32(Console.ReadLine());
-
-            //        HotDrink te = new Tea(water, produkt, "Tee", produktType, temp);
-            //        if (water <= 10 || produkt <= 12)
-            //        {
-            //            gui.Replenishment(te.CheckContents());
-            //        }
-            //        else
-            //        {
-            //            Console.WriteLine(te.MakeDrink());
-            //            Console.WriteLine(te.ServeDrink());
-            //        }
-            //        break;
-
-            //    case 3:
-            //        HotDrink espresso = new CoffeeMachine(water, produkt, "Espresso", "Coffee beanies", 96);
-            //        if (water <= 3 || produkt <= 5)
-            //        {
-            //            gui.Replenishment(espresso.CheckContents());
-            //        }
-            //        else
-            //        {
-            //            Console.WriteLine(espresso.MakeDrink());
-            //            Console.WriteLine(espresso.ServeDrink());
-            //        }
-            //        break;
-
-            //    default:
-            //        Console.WriteLine("Please enter a valid number ");
-            //        break;
-            //}
             Console.ReadKey();
         }
     }
